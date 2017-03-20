be6610aa4ad53e6d8d718cd0e3944252f7bde289
Mrinal Pande <me.mrinal@yahoo.com>
2017-03-20 19:44:12 +0530

    SVM
    Udacity's SVM

---
+++ Udacity/svm/svm_author_id.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+#########################################################
+### your code goes here ###
+
+clf = SVC(C=10000.0, kernel="rbf")
+
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+
+acc = accuracy_score(pred, labels_test)
+print "accuracy", acc
+
+count = 0
+
+for val in pred:
+    if val == 1:
+        count += 1
+
+print "count",count
+
+def submitAccuracy():
+    return acc
+#########################################################
+
+
